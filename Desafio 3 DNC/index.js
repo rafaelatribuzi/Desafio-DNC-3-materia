7165d61d1789e7bf419b0e912fba4f25b0766433
Rafaela Tribuzi <rafaela.tribuzilula@gmail.com>
2025-03-20 10:28:15 -0300

    Comitando

---
+++ Desafio 3 DNC/index.js
@@ -0,0 +1,7 @@
+let tasks = [
+    { id: 1, description: 'Comprar pão', checked: false},
+    { id: 1, description: 'Passear crianças', checked: false},
+    { id: 1, description: 'Fazer o almoço', checked: false},
+]
+
+window.onload
