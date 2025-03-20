dea8b1968e7b1ef2b72f1f49043108a8b1780a70
Rafaela Tribuzi <rafaela.tribuzilula@gmail.com>
2025-03-20 15:27:51 -0300

    comitando

--- Desafio 3 DNC/index.js
+++ Desafio 3 DNC/index.js
@@ -4,4 +4,42 @@ let tasks = [
     { id: 1, description: 'Fazer o almoço', checked: false},
 ]
 
-window.onload
+    const getCheckBoxInput = ({id, description, checked}) => {
+        const checkbox = document.createElement ('input');
+        const label = document.createElement ('label');
+        const wrapper = document.createElement ('div'); 
+        const checkboxId = `${id} - checkbox`;
+
+        checkbox.type = 'checkbox';
+        checkbox.id = checkboxId;
+        checkbox.checked = checked;
+        
+        label.textContent = description;
+        label.htmlFor = checkboxId;
+
+
+        wrapper.className = 'checkbox-label-container';
+        wrapper.appendChild (checkbox);
+        wrapper.appendChild (label);
+
+        return wrapper;
+
+    }
+
+
+window.onload = function() {
+    tasks.forEach ((task) => {
+        const checkbox = getCheckBoxInput(task);
+        
+        const list = document.getElementById ('todo-list');
+        const toDo = document.createElement ('li');
+
+        toDo.id = task.id;
+        toDo.appendChild (checkbox);
+
+        list.appendChild (toDo);
+
+       
+
+    })
+}
