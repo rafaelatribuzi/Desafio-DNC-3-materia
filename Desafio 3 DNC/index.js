8d8a27c5527f1770a018ef872a78e21e75c0a41f
Rafaela Tribuzi <rafaela.tribuzilula@gmail.com>
2025-03-21 11:13:28 -0300

    comitando

--- Desafio 3 DNC/index.js
+++ Desafio 3 DNC/index.js
@@ -1,4 +1,114 @@
-let tasks = [
+let completedTasks = 0;
+
+document.addEventListener("DOMContentLoaded", function () {
+document.getElementById("create-todo-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addTask();
+    });
+});
+
+
+function addTask() {
+    //Captura dos Inputs
+
+    let taskInput = document.getElementById('taskInput');
+    let tagInput = document.getElementById('tagInput');
+    let taskText = taskInput.value.trim();
+    let tagText = tagInput.value.trim();
+    
+    if(taskText === "") return;
+
+    //Criação do Item da Lista
+
+    let taskList = document.getElementById ('taskList');
+    let listItem = document.createElement ('li');
+
+    //Adicionando Data e Formatação
+
+    let date = new Date().toLocaleString();
+    let taskHeader = document.createElement('div');
+    taskHeader.classList.add('task-header');
+    taskHeader.innerHTML = `<span>${taskText} <small> (${date}) </small></span> <strong>${tagText ? '[' + tagText + ']' : ''}</strong>`;
+
+    // Criando o Botão "Excluir"
+
+    /*let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Excluir";
+    deleteBtn.onclick = function () {
+        listItem.classList.add ('done');
+    };*/
+
+    //Criando o Botão "Concluir"
+
+    let completedBtn = document.createElement ('button');
+    completedBtn.textContent = 'Concluir';
+    completedBtn.onclick = function () {
+        if (!listItem.classList.contains('done')) {
+            listItem.classList.add('done');
+            taskHeader.style.textDecoration = 'line-through';
+            completedTasks++;
+            completedBtn.innerHTML = '✔️';
+            completedBtn.disabled = true;
+        }
+        document.getElementById('completedCount').textContent = completedTasks;
+    };
+
+
+    // Montando e adicionado à Lista
+
+    listItem.appendChild(taskHeader);
+    //listItem.appendChild(deleteBtn);
+    listItem.appendChild(completedBtn);
+    taskList.appendChild(listItem);
+
+
+    // Limpando os campos
+
+    taskInput.value = "";
+    tagInput.value = "";    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let tasks = [
     { id: 1, description: 'Comprar pão', checked: false},
     { id: 1, description: 'Passear crianças', checked: false},
     { id: 1, description: 'Fazer o almoço', checked: false},
@@ -33,13 +143,15 @@ window.onload = function() {
         
         const list = document.getElementById ('todo-list');
         const toDo = document.createElement ('li');
+        //const button = document.createElement ('button');
 
         toDo.id = task.id;
         toDo.appendChild (checkbox);
+        // toDo.appendChild(button);
 
         list.appendChild (toDo);
 
        
 
     })
-}
+}*/
